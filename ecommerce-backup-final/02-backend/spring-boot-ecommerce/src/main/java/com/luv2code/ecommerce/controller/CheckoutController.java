15dc7f0dcd0f0245c7a614f8a462db70ffcfd026
BenjaminShumate <benjamin.shumate@hcl.com>
2022-06-10 16:55:30 -0500

    Final variant of ecommerce project pre-ammendments but with file logging

--- ecommerce-backup-final/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ ecommerce-backup-final/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -3,6 +3,7 @@ package com.luv2code.ecommerce.controller;
 import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
+import com.luv2code.ecommerce.entity.OrderItem;
 import com.luv2code.ecommerce.service.CheckoutService;
 import com.stripe.exception.StripeException;
 import com.stripe.model.PaymentIntent;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
+
+    private static final Logger logger = LogManager.getLogger(); //Added for logging purposes
 
     private CheckoutService checkoutService;
 
@@ -25,6 +29,15 @@ public class CheckoutController {
 
     @PostMapping("/purchase")
     public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        // Logging statement for placing an order
+        logger.info("Placing order for "+purchase.getCustomer());
+        logger.info("Sending order to "+purchase.getShippingAddress());
+        logger.info("Charging " + purchase.getOrder().getTotalPrice() + " to " + purchase.getBillingAddress());
+        logger.info("Generated tracking number "+purchase.getOrder().getOrderTrackingNumber());
+        for(OrderItem item: purchase.getOrderItems()){
+            logger.info("Buying Item in order product id "+ item.getProductId());
+        }
 
         PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
